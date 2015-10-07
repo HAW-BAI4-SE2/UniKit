4d5da875e1ff44eabe7c2fcd3387e98a641cb3f4
MrTorgue <andreas.berks@haw-hamburg.de>
2015-10-07 19:18:18 +0200

    Added utility class prototype for import interface. Remove deprecated interface agreements from documents.

---
+++ project/app/models/imports/implementations/Values.java
@@ -0,0 +1,20 @@
+package models.imports.implementations;
+
+import models.imports.interfaces.Course;
+import models.imports.interfaces.Student;
+
+import java.util.Collection;
+
+public class Values {
+    public static Student getCurrentUser() {
+        return null;
+    }
+
+    public static Collection<Student> getStudents() {
+        return null;
+    }
+
+    public static Collection<Course> getCourses() {
+        return null;
+    }
+}
