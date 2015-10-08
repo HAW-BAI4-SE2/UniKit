6561f6a07ea7deeb15e583d7ae2ae0ef6c52d099
Thomas <tbednorz@posteo.de>
2015-10-08 16:31:19 +0200

    - Developer-Branch thomas_branch erstellt. - (Leerer) Controller für Veranstaltungsanmeldung (courseEnrollment) erstellt.

---
+++ project/app/controllers/courseEnrollment.java
@@ -0,0 +1,12 @@
+package controllers;
+
+/**
+ * Created by Thomas Bednorz on 10/8/2015.
+ */
+
+import play.*;
+import play.mvc.*;
+import views.html.*;
+
+public class courseEnrollment extends Controller {
+}
