ba20f03932cc7f7fe24920e843a0f3bf9f0e124e
Thomas Bednorz <tbednorz@posteo.de>
2015-10-09 18:38:47 +0200

    Added dummies for courses and students

---
+++ project/app/models/dummies/DummyStudent.java
@@ -0,0 +1,52 @@
+package models.dummies;
+
+import models.imports.interfaces.Course;
+import models.imports.interfaces.Student;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by Thomas Bednorz on 10/8/2015.
+ */
+public class DummyStudent implements Student {
+
+    Collection<Course> availableCourses = new ArrayList<Course>();
+
+    public DummyStudent() {
+        Course rn = new DummyCourse(42,"Rechnernetze","RN",2,3);
+        Course se2 = new DummyCourse(23,"Software Engineering 2","SE2",2,3);
+        availableCourses.add(rn);
+        availableCourses.add(se2);
+    }
+
+    @Override
+    public String getStudentNumber() {
+        return "666";
+    }
+
+    @Override
+    public String getFirstName() {
+        return "Horst";
+    }
+
+    @Override
+    public String getLastName() {
+        return "Local";
+    }
+
+    @Override
+    public String getEmail() {
+        return "localhorst@unikit.de";
+    }
+
+    @Override
+    public int getSemester() {
+        return 4;
+    }
+
+    @Override
+    public Collection<Course> getAvailableCourses() {
+        return availableCourses;
+    }
+}
