4b43fd8367f5ba6594fc0ea01c3d2155b1e8ecbc
JonesPButter <Jonas.j@hotmail.de>
2015-10-11 21:24:16 +0200

    Values bearbeitet - getCurrentUser() muss noch bearbeitet werden/Primary Key bzw. Matrikelnummer unbekannt von User: Jonas. Es sollte ja irgendein User aus der Datenbank geladen werden zum Test.

--- project/app/models/imports/implementations/Values.java
+++ project/app/models/imports/implementations/Values.java
@@ -1,20 +1,99 @@
 package models.imports.implementations;
 
+import models.imports.implementations.hibernate.CourseModel;
+import models.imports.implementations.hibernate.StudentModel;
 import models.imports.interfaces.Course;
 import models.imports.interfaces.Student;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 public class Values {
+
+    public static SessionFactory createSessionFactory() {
+        SessionFactory sessionFactory;
+        ServiceRegistry serviceRegistry;
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new ServiceRegistryBuilder().applySettings(
+                configuration.getProperties()). buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
+    }
+
     public static Student getCurrentUser() {
-        return null;
+        SessionFactory factory = createSessionFactory();
+        Session session = null;
+        int matrikelnummer = 1;
+        Student student = null;
+
+        try{
+            session = factory.openSession();
+            StudentModel studentModel = ((StudentModel)session.load(StudentModel.class,matrikelnummer));
+            student = new StudentImpl(studentModel);
+        }
+        finally {
+            if (session != null && session.isConnected()) {
+                session.close();
+            }
+        }
+        return student;
     }
 
     public static Collection<Student> getStudents() {
-        return null;
+        SessionFactory factory = createSessionFactory();
+        Session session = null;
+        List<Student> students = new ArrayList<>();
+
+        session = factory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            List<StudentModel> studentList = session.createQuery("FROM Student").list();
+            for (Iterator iterator=studentList.iterator(); iterator.hasNext();){
+                StudentModel studentModel = (StudentModel) iterator.next();
+                students.add(new StudentImpl(studentModel));
+            }
+            tx.commit();
+        }catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+        return students;
     }
 
     public static Collection<Course> getCourses() {
-        return null;
+        SessionFactory factory = createSessionFactory();
+        Session session = null;
+        List<Course> courses = new ArrayList<>();
+
+        session = factory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            List<Course> courseList = session.createQuery("FROM Course").list();
+            for (Iterator iterator=courseList.iterator(); iterator.hasNext();){
+                CourseModel courseModel = (CourseModel) iterator.next();
+                courses.add(new CourseImpl(courseModel));
+            }
+            tx.commit();
+        }catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+        return courses;
     }
 }
