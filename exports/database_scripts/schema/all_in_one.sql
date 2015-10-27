f5dced8b3268eec0a6be84252b941c43ff793fc7
MrTorgue <andreas.berks@haw-hamburg.de>
2015-10-28 00:15:32 +0100

    Added field currently_assigned_to_team in table COURSE_REGISTRATION. Added creation and update information in UniKit database.

---
+++ exports/database_scripts/schema/all_in_one.sql
@@ -0,0 +1,5 @@
+DROP DATABASE `import_database`;
+DROP DATABASE `unikit_database`;
+
+CREATE SCHEMA `import_database`;
+CREATE SCHEMA `unikit_database`;
--- exports/database_scripts/unikit_database/all_in_one.sql
+++ exports/database_scripts/unikit_database/all_in_one.sql
@@ -1,18 +1,38 @@
-DROP TABLE `unikit_database`.`COURSE_REGISTRATION`;
+DROP TABLE `unikit_database`.`TEAM_APPLICATION`;
+DROP TABLE `unikit_database`.`TEAM_INVITATION`;
 DROP TABLE `unikit_database`.`TEAM_REGISTRATION`;
 DROP TABLE `unikit_database`.`TEAM`;
+DROP TABLE `unikit_database`.`COURSE_REGISTRATION`;
+
+CREATE TABLE `unikit_database`.`COURSE_REGISTRATION` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `student_number` VARCHAR(31) NOT NULL COMMENT '',
+  `course_id` INT NOT NULL COMMENT '',
+  `currently_assigned_to_team` BIT NOT NULL DEFAULT 0 COMMENT '',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC)  COMMENT '',
+  INDEX `student_number_idx` (`student_number` ASC)  COMMENT '',
+  INDEX `course_id_idx` (`course_id` ASC)  COMMENT '');
 
 CREATE TABLE `unikit_database`.`TEAM` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `course_id` INT NOT NULL COMMENT '',
+  `created_by_student_number` VARCHAR(31) NOT NULL COMMENT '',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '',
   UNIQUE INDEX `id_UNIQUE` (`id` ASC)  COMMENT '',
-  INDEX `course_id_idx` (`course_id` ASC)  COMMENT '');
+  INDEX `course_id_idx` (`course_id` ASC)  COMMENT '',
+  INDEX `created_by_student_number_idx` (`created_by_student_number` ASC)  COMMENT '');
 
 CREATE TABLE `unikit_database`.`TEAM_REGISTRATION` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `student_number` VARCHAR(31) NOT NULL COMMENT '',
   `team_id` INT NOT NULL COMMENT '',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '',
   UNIQUE INDEX `id_UNIQUE` (`id` ASC)  COMMENT '',
   INDEX `student_number_idx` (`student_number` ASC)  COMMENT '',
@@ -23,11 +43,36 @@ CREATE TABLE `unikit_database`.`TEAM_REGISTRATION` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-CREATE TABLE `unikit_database`.`COURSE_REGISTRATION` (
+CREATE TABLE `unikit_database`.`TEAM_INVITATION` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `student_number` VARCHAR(31) NOT NULL COMMENT '',
-  `course_id` INT NOT NULL COMMENT '',
+  `team_id` INT NOT NULL COMMENT '',
+  `created_by_student_number` VARCHAR(31) NOT NULL COMMENT '',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '',
   UNIQUE INDEX `id_UNIQUE` (`id` ASC)  COMMENT '',
   INDEX `student_number_idx` (`student_number` ASC)  COMMENT '',
-  INDEX `course_id_idx` (`course_id` ASC)  COMMENT '');
+  INDEX `team_id_idx` (`team_id` ASC)  COMMENT '',
+  INDEX `created_by_student_number_idx` (`created_by_student_number` ASC)  COMMENT '',
+  CONSTRAINT `team_id_team_invitation`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `unikit_database`.`TEAM` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `unikit_database`.`TEAM_APPLICATION` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `student_number` VARCHAR(31) NOT NULL COMMENT '',
+  `team_id` INT NOT NULL COMMENT '',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC)  COMMENT '',
+  INDEX `student_number_idx` (`student_number` ASC)  COMMENT '',
+  INDEX `team_id_idx` (`team_id` ASC)  COMMENT '',
+  CONSTRAINT `team_id_team_application`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `unikit_database`.`TEAM` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
