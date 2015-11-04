8ca58f22f0a468cbbea2f1ca77544d0aa9114f23
tbednorz <tbednorz@gmx.de>
2015-11-04 20:43:56 +0100

    Updated code for Phase 2

--- project/app/controllers/CourseController.java
+++ project/app/controllers/CourseController.java
@@ -4,6 +4,12 @@ package controllers;
  * @author Thomas Bednorz
  */
 
+import assets.Global;
+
+import net.unikit.database.external.interfaces.Course;
+import net.unikit.database.external.interfaces.Student;
+import net.unikit.database.unikit_.interfaces.CourseRegistration;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -12,17 +18,21 @@ import views.html.*;
 public class CourseController extends Controller {
 
     /**
-     * Creates a team for the course with the user as the first member
+     * Creates a team associated with the courseID and studentID
      * @return showEditTeam-page displaying options for inviting other students, cancelling existing invitations,
      * accepting/denying MembershipRequests and deleting the team
      */
     public static Result createTeam(){
-        //TODO: Receives courseID and studentNumber, creates team in database
+        /* TODO: 
+                create team using courseID and studentNumber
+                display showEditTeam-page
+
+        */
         return ok(showEditTeam.render());
     }
 
     /**
-     *
+     * Deletes the team associated with the studentNumber and courseID
      * @return showCourseDetails-page displaying details for the course
      */
     public static Result deleteTeam(){
--- project/app/controllers/TeamController.java
+++ project/app/controllers/TeamController.java
@@ -11,89 +11,113 @@ import views.html.*;
 
 public class TeamController extends Controller {
 
-    public static Result deleteMember(){
-        /* TODO:
-                delete studentNumber from TEAM_REGISTRATION
-                send mail to student
-                display EditTeam-page
-         */
-        return ok(showEditTeam.render());
-    }
 
     private static Result addMember() {
         /* TODO:
-                persist studentNumber to TEAM_REGISTRATION
-                display EditTeam-page
+         *      persist studentNumber to TEAM_REGISTRATION
+         *      send mail to new member
+         *      send mail to other team members
          */
-        return ok(showEditTeam.render());
+        return showEditTeam();
+    }
+
+    public static Result deleteMember(){
+        /* TODO:
+         *      delete studentNumber from TEAM_REGISTRATION
+         *      send mail to former member
+         *      send mail to other members
+         */
+        return showEditTeam();
     }
 
     public static Result inviteStudent(){
         /* TODO:
-                persist TeamInvitation for studentNumber and teamID
+                persist invitation to database
                 send mail to student
                 send mail to teammembers
-                display EditTeam-page
          */
-        return ok(showEditTeam.render());
+         return showEditTeam();
     }
+
+    public static Result revokeInvitaion(){
+        /*
+        *   TODO:
+        *       delete invitation from database
+        *       send mail to invited student
+        *       send mail to team members
+        */
+        return showEditTeam();
+    }
+
+    public static Result acceptInvitation(){
+        /* TODO:
+         *      delete invitation from database
+         */
+        return addMember();
+    }
+
+    /**
+    *   Declines (deletes) the invitation for the student by the team
+    *   @param teamID the ID of the team that issued the invite
+    *   @param studentNumber the student number of the student who declined the invitation
+    **/
+    public static Result declineInvitation(){
+        /* TODO
+         *      delete invitation from database
+         *      send mail to team members
+         *      send mail to student
+         */
+        return showEditTeam();
+    }
+
 
     public static Result acceptMembershipRequest(){
         /* TODO:
-                delete invite
-                send mail to requesting student
-                send mail to all members of the team
-                add the student
+                delete membershiprequest from database
          */
         return addMember();
     }
 
     public static Result requestMembership(){
         /* TODO:
-                Persist request for studentNumber and teamID
+                persist membershiprequest in database
                 send mail to student and potential teammembers
-                display CourseDetail-page
+         */
+        return ok(showCourseDetails.render());
+    }
+
+    /**
+     *  Revokes (deletes) the MembershipRequest associated with the studentNumber and teamID and displays the results
+     *  @param teamID the teamID of the team for which the membership request was issued
+     *  @param studentNumber the studentNumber of the student who issued the membership request
+    **/
+    public static Result cancelMembershipRequest(){
+        /* TODO:
+         *      delete membership request from database
+         *      send mail to team members
+         *      send mail to student
+         *      display course detail page
          */
         return ok(showCourseDetails.render());
     }
 
     public static Result showTeamOverview(){
         /* TODO:
-                display all registered teams
-                display all pending requests
-                display all pending invites
+         *      display all registered teams
+         *      display all pending requests
+         *      display all pending invites
          */
         return ok(showTeamOverview.render());
     }
 
-    public static Result acceptInvitation(){
-        /* TODO:
-                delete invite
-                send mail to team members
-                send mail to student
-                add student to team
-         */
-        return addMember();
-    }
-
-    public static Result declineInvitation(){
-        /* TODO
-                delete invitation
-                send mail to team members
-                send mail to student
-                display team overview
-         */
-        return ok(showCourseDetails.render());
-    }
-
-    public static Result deleteMembershipRequest(){
-    /* TODO:
-            delete membership request
-            send mail to team members
-            send mail to student
-            display course detail page
-     */
-        return ok(showCourseDetails.render());
+    public static Result showEditTeam(){
+        /**
+        *   TODO:
+        *       display all members of the team
+        *       display all pending requests
+        *       display all pending invitations
+        **/
+        return ok(showEditTeam.render());
     }
 
     /**
