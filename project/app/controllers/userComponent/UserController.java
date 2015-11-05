ae6fea6eff7206f018be2768caee19edb26e9933
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-05 23:54:46 +0100

    Added view showUser.

--- project/app/controllers/userComponent/UserController.java
+++ project/app/controllers/userComponent/UserController.java
@@ -1,19 +1,24 @@
 package controllers.userComponent;
 
-import models.loginComponent.LoginFormModel;
-import play.data.Form;
+import assets.Global;
+import net.unikit.database.external.interfaces.Student;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
+
+import java.util.Date;
 
 /**
  * Created by Andreas on 05.11.2015.
  */
 public class UserController extends Controller {
     public static Result showUser() {
-        LoginFormModel loginFormModel = new LoginFormModel("", "");
-        Form<LoginFormModel> loginForm = Form.form(LoginFormModel.class).fill(loginFormModel);
+        Student currentUser = Global.getStudentManager().getStudent("2055120");
+        
+        Date sessionTimeoutDate = new Date();
+        java.util.Calendar sessionTimeout = java.util.Calendar.getInstance();
+        sessionTimeout.setTime(sessionTimeoutDate);
 
-        return ok(showUser.render(loginForm));
+        return ok(showUser.render(currentUser, sessionTimeout));
     }
 }
