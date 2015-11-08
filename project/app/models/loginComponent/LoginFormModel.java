a3b3f9bbf4425d780aa2dd73a09237a981d65ce8
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-08 16:54:46 +0100

    Added password encryption.

--- project/app/models/loginComponent/LoginFormModel.java
+++ project/app/models/loginComponent/LoginFormModel.java
@@ -1,5 +1,6 @@
 package models.loginComponent;
 
+import assets.BCrypt;
 import assets.Global;
 import net.unikit.database.external.interfaces.Student;
 import play.data.validation.Constraints;
@@ -51,7 +52,8 @@ public class LoginFormModel {
         boolean usernameUnknown = (currentUser == null);
 
         // Check if password is wrong
-        boolean passwordWrong = false; // TODO: Implement check!
+        String hashed = BCrypt.hashpw("test", BCrypt.gensalt()); // TODO: Load password from DB!
+        boolean passwordWrong = !BCrypt.checkpw(password, hashed);
 
         // Return error if username is unknown or password is wrong
         if (usernameUnknown || passwordWrong)
