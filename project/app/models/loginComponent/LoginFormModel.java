ac20057ebad4c78d8f13063906395478266481e2
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-10 09:20:32 +0100

    Readded password check.

--- project/app/models/loginComponent/LoginFormModel.java
+++ project/app/models/loginComponent/LoginFormModel.java
@@ -37,8 +37,8 @@ public class LoginFormModel {
             errors.add(new ValidationError("", "Username wurde nicht angegeben!"));
 
         // TODO: Comment-In: Check if password is missing
-        //if (password == null || password.isEmpty())
-        //    errors.add(new ValidationError("", "Password wurde nicht angegeben!"));
+        if (password == null || password.isEmpty())
+            errors.add(new ValidationError("", "Password wurde nicht angegeben!"));
 
         // If errors occurred, return them
         if (!errors.isEmpty())
@@ -52,7 +52,7 @@ public class LoginFormModel {
 
         // Check if password is wrong
         String hashed = BCrypt.hashpw("test", BCrypt.gensalt()); // TODO: Load hashed password from DB!
-        boolean passwordWrong = false; //TODO: Change to: !BCrypt.checkpw(password, hashed);
+        boolean passwordWrong = !BCrypt.checkpw(password, hashed);
 
         // Return error if username is unknown or password is wrong
         if (usernameUnknown || passwordWrong)
